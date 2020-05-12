27033bfbc7aa4d8afaf9e0197f738c1cec01839f
Daniel Pipa <danielpipa21@gmail.com>
2020-05-12 11:24:02 -0500

    General settings CSS

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production';
+    console.log(devMode);
+    return {
+        entry: './src/app.js',
+        output: {
+            path: __dirname + '/build',
+            filename: 'bundle.js'
+        },
+        devServer: {
+            open: true,
+            port: 9000
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader },
+                        { loader: 'css-loader' },
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: '[name].[ext]',
+                                outputPath: 'static/',
+                                useRelativePath: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'bundle.css'
+            })
+        ]
+    }
+}
