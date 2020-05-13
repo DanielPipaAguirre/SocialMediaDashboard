cde97c1151001fd35a39d1abeb260830dd7d6814
Daniel Pipa <danielpipa21@gmail.com>
2020-05-13 11:43:07 -0500

    Dark mode

--- src/app.js
+++ src/app.js
@@ -5,3 +5,9 @@ import './img/icon-instagram.svg';
 import './img/icon-youtube.svg';
 import './img/icon-up.svg';
 import './img/icon-down.svg';
+
+const toggle = document.getElementById("checkbox");
+toggle.checked = false;
+toggle.addEventListener("change", () => {
+    document.body.classList.toggle('is-dark');
+})
